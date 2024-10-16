ec3b376e68c3966a9d155f80f88cc2b70b734333
Haothai1 <Hao2003thai@gmail.com>
2024-10-15 20:06:34 -0700

    inventory search, menu search, and store product search functionality with basic design

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,10 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
+import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+// Search filter function
+import SearchController from "./search_controller"
+application.register("search", SearchController)
+
+console.log("Search controller registered");
